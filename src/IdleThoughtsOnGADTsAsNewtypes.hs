243e935ed67a20f16b62b7b3866ab96bb2b0d6e6
Ryan Scott <ryan.gl.scott@gmail.com>
2020-02-20 09:44:56 -0500

    Make IdleThoughtsOnGADTsAsNewtypes work with simplified subsumption

--- src/IdleThoughtsOnGADTsAsNewtypes.hs
+++ src/IdleThoughtsOnGADTsAsNewtypes.hs
@@ -32,24 +32,24 @@ fromT1 :: forall a. T a -> ((a ~ Bool) => Bool)
 fromT1 = coerce @(T a) @((a ~ Bool) => Bool)
 
 fromT2 :: forall a. (a ~ Bool) => T a -> Bool
-fromT2 = coerce @(T a) @((a ~ Bool) => Bool)
+fromT2 t = coerce @(T a) @((a ~ Bool) => Bool) t
 
 fromT3 :: T Bool -> Bool
-fromT3 = coerce @(T Bool) @((Bool ~ Bool) => Bool)
+fromT3 t = coerce @(T Bool) @((Bool ~ Bool) => Bool) t
 
 toT1 :: forall a. ((a ~ Bool) => Bool) -> T a
 toT1 = coerce @((a ~ Bool) => Bool) @(T a)
 
 toT2 :: forall a. (a ~ Bool) => Bool -> T a
-toT2 = coerce @((a ~ Bool) => Bool) @(T a)
+toT2 b = coerce @((a ~ Bool) => Bool) @(T a) b
 
 toT3 :: Bool -> T Bool
-toT3 = coerce @((Bool ~ Bool) => Bool) @(T Bool)
+toT3 b = coerce @((Bool ~ Bool) => Bool) @(T Bool) b
 
 instance (a ~ Bool) => Eq (T a) where
   (==) = coerce @(((a ~ Bool) => Bool) -> ((a ~ Bool) => Bool) -> Bool)
                 @(T a                  -> T a                  -> Bool)
-                (==)
+                (\x y -> x == y)
 
 {-
 Can't do this:
@@ -65,19 +65,19 @@ fromS1 :: forall a. S a -> (forall a'. (a ~ Identity a') => a')
 fromS1 = coerce @(S a) @(forall a'. (a ~ Identity a') => a')
 
 fromS2 :: forall a a'. (a ~ Identity a') => S a -> a'
-fromS2 = coerce @(S a) @(forall a''. (a ~ Identity a'') => a'')
+fromS2 s = coerce @(S a) @(forall a''. (a ~ Identity a'') => a'') s
 
 fromS3 :: forall a. S (Identity a) -> a
-fromS3 = coerce @(S (Identity a)) @(forall a'. (Identity a ~ Identity a') => a')
+fromS3 s = coerce @(S (Identity a)) @(forall a'. (Identity a ~ Identity a') => a') s
 
 toS1 :: forall a. (forall a'. (a ~ Identity a') => a') -> S a
 toS1 = coerce @(forall a'. (a ~ Identity a') => a') @(S a)
 
 toS2 :: forall a a'. (a ~ Identity a') => a' -> S a
-toS2 = coerce @(forall a''. (a ~ Identity a'') => a'') @(S a)
+toS2 x = coerce @(forall a''. (a ~ Identity a'') => a'') @(S a) x
 
 toS3 :: forall a. a -> S (Identity a)
-toS3 = coerce @(forall a'. (Identity a ~ Identity a') => a') @(S (Identity a))
+toS3 x = coerce @(forall a'. (Identity a ~ Identity a') => a') @(S (Identity a)) x
 
 {-
 Can we do this?
@@ -93,7 +93,7 @@ fromO1 :: O -> (forall a. a)
 fromO1 = coerce @O @(forall a. a)
 
 fromO2 :: forall a. O -> a
-fromO2 = coerce @O @(forall a'. a')
+fromO2 o = coerce @O @(forall a'. a') o
 
 toO1 :: (forall a. a) -> O
 toO1 = coerce @(forall a. a) @O
@@ -130,13 +130,15 @@ fromSI1 = coerce @(Sing i)
 
 fromSI2 :: forall a (i :: Identity a) (x :: a). (i ~ 'Identity x)
         => Sing i -> Sing x
-fromSI2 = coerce @(Sing i)
-                 @(forall (x' :: a). (i ~ 'Identity x') => Sing x')
+fromSI2 si = coerce @(Sing i)
+                    @(forall (x' :: a). (i ~ 'Identity x') => Sing x')
+                    si
 
 fromSI3 :: forall a (x :: a).
            Sing ('Identity x) -> Sing x
-fromSI3 = coerce @(Sing ('Identity x))
-                 @(forall (x' :: a). ('Identity x ~ 'Identity x') => Sing x')
+fromSI3 si = coerce @(Sing ('Identity x))
+                    @(forall (x' :: a). ('Identity x ~ 'Identity x') => Sing x')
+                    si
 
 toSI1 :: forall a (i :: Identity a).
          (forall (x :: a). (i ~ 'Identity x) => Sing x) -> Sing i
@@ -145,11 +147,13 @@ toSI1 = coerce @(forall (x' :: a). (i ~ 'Identity x') => Sing x')
 
 toSI2 :: forall a (i :: Identity a) (x :: a). (i ~ 'Identity x)
       => Sing x -> Sing i
-toSI2 = coerce @(forall (x' :: a). (i ~ 'Identity x') => Sing x')
-               @(Sing i)
+toSI2 sx = coerce @(forall (x' :: a). (i ~ 'Identity x') => Sing x')
+                  @(Sing i)
+                  sx
 
 toSI3 :: forall a (x :: a).
          Sing x -> Sing ('Identity x)
-toSI3 = coerce @(forall (x' :: a). ('Identity x ~ 'Identity x') => Sing x')
-               @(Sing ('Identity x))
+toSI3 sx = coerce @(forall (x' :: a). ('Identity x ~ 'Identity x') => Sing x')
+                  @(Sing ('Identity x))
+                  sx
 #endif
