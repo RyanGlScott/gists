a311d4c691df208fc4cea4fed715783f23a851be
Ryan Scott <ryan.gl.scott@gmail.com>
2020-10-01 07:25:49 -0400

    Use VDQ in ElimNat.hs and S.hs
    Unfortunately, the KindOf hack that I was using to backport this
    trick to old versions of GHC doesn't work under Quick Look. (See the
    "QL isn't applied in kinds at all" bullet point of GHC#18126.) Let's
    just bite the bullet and use proper VDQ, restricting these modules to
    only being buildable with GHC 8.10 or later.

--- src/ElimNat.hs
+++ src/ElimNat.hs
@@ -1,51 +1,73 @@
-{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE CPP #-}
+
+#if __GLASGOW_HASKELL__ >= 810
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE ImpredicativeTypes #-}
 {-# LANGUAGE LiberalTypeSynonyms #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeInType #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
+#endif
 module ElimNat where
 
+#if __GLASGOW_HASKELL__ >= 810
 import Data.Kind
 
 -- | A type-level eliminator for 'Nat's.
-type family ElimNat (p :: Nat ~> Type)
-                    (s :: Nat)
-                    (pZ :: p @@ Z)
-                    (pS :: KindOf (PS p)) -- (pS :: forall (n :: Nat) -> (p @@ n ~> p @@ S n))
-                 :: p @@ s where
-  { ElimNat _ Z     pZ _  = pZ ;
-#if __GLASGOW_HASKELL__ >= 808
-    forall (p :: Nat ~> Type) (n :: Nat) (pZ :: p @@ Z) (pS :: KindOf (PS p)).
-#endif
+type ElimNat :: forall (p :: Nat ~> Type) (s :: Nat)
+             -> p @@ Z
+             -> (forall (n :: Nat) -> p @@ n ~> p @@ S n)
+             -> p @@ s
+type family ElimNat p s pZ pS where
+  forall (p :: Nat ~> Type)
+         (pZ :: p @@ Z) (pS :: forall (n :: Nat) -> p @@ n ~> p @@ S n).
+    ElimNat p Z pZ pS = pZ
+  forall (p :: Nat ~> Type)
+         (pZ :: p @@ Z) (pS :: forall (n :: Nat) -> p @@ n ~> p @@ S n)
+         (n :: Nat).
     ElimNat p (S n) pZ pS = pS n @@ ElimNat p n pZ pS
-  }
-data PS (p :: Nat ~> Type) (n :: Nat) :: p @@ n ~> p @@ S n
 
 -- | Multiply a 'Nat' by two.
-type family TimesTwo (n :: Nat) :: Nat where
+type TimesTwo :: Nat -> Nat
+type family TimesTwo n where
   TimesTwo n = ElimNat (ConstSym1 Nat) n Z Doubler
+
+type Doubler :: forall (n :: Nat) -> Const Nat n ~> Nat
 data Doubler (n :: Nat) :: Const Nat n ~> Nat
 type instance Doubler _ @@ x = S (S x)
 
-data TyFun :: Type -> Type -> Type
+type TyFun :: Type -> Type -> Type
+data TyFun a b
+
+type (~>) :: Type -> Type -> Type
 type a ~> b = TyFun a b -> Type
 infixr 0 ~>
-type family (f :: a ~> b) @@ (x :: a) :: b
+
+type (@@) :: (a ~> b) -> a -> b
+type family f @@ x
+
+type KindOf :: k -> Type
 type KindOf (a :: k) = k
 
-data family Sing (z :: k)
+type Sing :: k -> Type
+data family Sing
 
+type Nat :: Type
 data Nat = Z | S Nat
-data instance Sing (z :: Nat) where
+
+data instance Sing :: Nat -> Type where
   SZ :: Sing Z
   SS :: Sing n -> Sing (S n)
 
-data ConstSym1 :: forall a b. a -> b ~> a
+type ConstSym1 :: a -> b ~> a
+data ConstSym1 a f
 type instance ConstSym1 x @@ y = Const x y
-type family Const (x :: a) (y :: b) :: a where
+
+type Const :: a -> b -> a
+type family Const x y where
   Const x _ = x
+#endif
--- src/S.hs
+++ src/S.hs
@@ -1,59 +1,58 @@
+{-# LANGUAGE CPP #-}
+
+#if __GLASGOW_HASKELL__ >= 810
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE ImpredicativeTypes #-}
 {-# LANGUAGE LiberalTypeSynonyms #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeInType #-}
 {-# LANGUAGE TypeOperators #-}
+#endif
 -- | Conor McBride's S combinator in GHC
 module S where
 
+#if __GLASGOW_HASKELL__ >= 810
 import Data.Kind
 
-data TyFun :: Type -> Type -> Type
+type TyFun :: Type -> Type -> Type
+data TyFun a b
+
+type (~>) :: Type -> Type -> Type
 type a ~> b = TyFun a b -> Type
 infixr 0 ~>
-type family (f :: a ~> b) @@ (x :: a) :: b
+
+type (@@) :: (a ~> b) -> a -> b
+type family f @@ x
+
+type KindOf :: k -> Type
 type KindOf (a :: k) = k
 
 -- | Warm-up.
-type DApply a
-            (b :: a ~> Type)
-            (f :: KindOf (DApplyAux a b))
-              -- (f :: forall (x :: a) -> b @@ x)
-            (x :: a)
-  = (f x :: b @@ x)
-type family DApplyAux a (b :: a ~> Type) (x :: a) :: b @@ x where {}
+type DApply :: forall a
+                      (b :: a ~> Type)
+            -> (forall (x :: a) -> b @@ x)
+            -> forall (x :: a)
+            -> b @@ x
+type DApply a b f x = f x
 
 -- | Conor McBride's S combinator.
-type S a
-       (b :: a ~> Type)
-       (c :: KindOf (SAux1 a b))
-         -- (c :: forall (x :: a) -> (b @@ x ~> Type))
-       (f :: KindOf (SAux2 a b c))
-         -- (f :: forall (x :: a) (y :: b @@ x) -> c x @@ y)
-       (g :: KindOf (SAux3 a b))
-         -- (g :: forall (x :: a) -> b @@ x)
-       (x :: a)
-  = (f x (g x) :: c x @@ g x)
-type family SAux1 a (b :: a ~> Type)
-                  (x :: a) :: b @@ x ~> Type
-type family SAux2 a (b :: a ~> Type) (c :: KindOf (SAux1 a b))
-                  (x :: a) (y :: b @@ x) :: c x @@ y
-type SAux3 a (b :: a ~> Type) (x :: a) = (DApplyAux a b x :: b @@ x)
+type S :: forall a
+                 (b :: a ~> Type)
+                 (c :: forall (x :: a) -> (b @@ x ~> Type))
+       -> (forall (x :: a) (y :: b @@ x) -> c x @@ y)
+       -> forall (g :: forall (x :: a) -> b @@ x)
+                 (x :: a)
+       -> c x @@ g x
+type S a b c f g x = f x (g x)
 
 -- | Depedent function composition (in limited form).
-type DComp a
-           (b :: a -> Type)
-           (c :: forall (x :: a). KindOf (DCompAux1 a b x))
-             -- (c :: forall (x :: a). b x -> Type)
-           (f :: forall (x :: a). KindOf (DCompAux2 a b c x))
-             -- (f :: forall (x :: a). forall (y :: b x) -> c y)
-           (g :: KindOf (DCompAux3 a b))
-             -- (g :: forall (x :: a) -> b x)
-           (x :: a)
-  = (f (g x) :: c (g x))
-type family DCompAux1 a (b :: a -> Type)
-                      (x :: a) :: b x -> Type
-type family DCompAux2 a (b :: a -> Type) (c :: forall (x :: a). KindOf (DCompAux1 a b x))
-                      (x :: a) (y :: b x) :: c y
-type family DCompAux3 a (b :: a -> Type) (x :: a) :: b x
+type DComp :: forall a
+                     (b :: a -> Type)
+                     (c :: forall (x :: a). b x -> Type)
+           -> (forall (x :: a). forall (y :: b x) -> c y)
+           -> forall (g :: forall (x :: a) -> b x)
+                     (x :: a)
+           -> c (g x)
+type DComp a b c f g x = f (g x)
+#endif
